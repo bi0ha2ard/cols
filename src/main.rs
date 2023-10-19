eec0dd7e639dcae3d2f11adf2962058b5a7811f7
Felix <felix@fzeltner.de>
2023-10-20 00:55:32 +0200

    feat: implement command line parsing and fix discovery

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+use itertools::Itertools;
 use serde::Deserialize;
 use serde_xml_rs::from_reader;
 use std::fs::{self, File};
 use std::io::{self, BufReader};
 use std::path::{Path, PathBuf};
 
+fn default_build_type() -> String {
+    "ros.catkin".to_string()
+}
+
+fn default_export() -> Export {
+    Export{ build_type: default_build_type() }
+}
+
 #[derive(Deserialize)]
 struct Export {
+    #[serde(default = "default_build_type")]
     build_type: String,
 }
 
 #[derive(Deserialize)]
 struct Package {
     name: String,
+    #[serde(default = "default_export")]
     export: Export,
 }
 
@@ -30,10 +42,20 @@ impl Entry {
             self.pkg.export.build_type,
         );
     }
+
+    fn print_from_opts(&self, args: &ListArgs) {
+        if args.names_only {
+            println!("{}", self.pkg.name);
+        } else if args.paths_only {
+            println!("{}", self.path.to_string_lossy());
+        } else {
+            self.print();
+        }
+    }
 }
 
 enum SearchOutcome {
-    Found { entry: Entry },
+    Found(Entry),
     Ignored,
     IsFile,
     Recurse,
@@ -41,6 +63,7 @@ enum SearchOutcome {
 
 static IGNORE_MARKERS: [&str; 3] = ["COLCON_IGNORE", "CATKIN_IGNORE", "AMENT_IGNORE"];
 
+// TODO: follow symlinks?
 fn check_path(dir: &PathBuf) -> SearchOutcome {
     use SearchOutcome::*;
     if !dir.is_dir() {
@@ -64,13 +87,16 @@ fn check_path(dir: &PathBuf) -> SearchOutcome {
 
     let pkg_xml = dir.join("package.xml");
     if pkg_xml.exists() {
-        if let Ok(pkg) = parse_package(&pkg_xml) {
-            return Found {
-                entry: Entry {
+        match parse_package(&pkg_xml) {
+            Ok(pkg) => {
+                return Found(Entry {
                     pkg,
                     path: dir.clone(),
-                },
-            };
+                });
+            }
+            Err(_e) => {
+                // eprintln!("Could not parse '{:?}': {}", pkg_xml, _e);
+            }
         }
     }
     return Recurse {};
@@ -83,7 +109,12 @@ fn parse_package(xml_file: &PathBuf) -> Result<Package> {
     return Ok(p);
 }
 
-fn find_packages(dir: &Path, results: &mut Vec<Entry>, immediate: bool) -> io::Result<()> {
+fn find_packages(
+    dir: &Path,
+    results: &mut Vec<Entry>,
+    args: &ListArgs,
+    recurse: bool,
+) -> io::Result<()> {
     if !dir.is_dir() {
         return Ok(());
     }
@@ -92,15 +123,11 @@ fn find_packages(dir: &Path, results: &mut Vec<Entry>, immediate: bool) -> io::R
         if let Ok(e) = entry {
             let check_outcome = check_path(&e.path());
             match check_outcome {
-                Found { entry: pkg } => {
-                    if immediate {
-                        pkg.print();
-                    } else {
-                        results.push(pkg);
-                    }
+                Found(entry) => {
+                    results.push(entry);
                 }
-                Recurse => {
-                    find_packages(&e.path(), results, immediate)?;
+                Recurse if recurse => {
+                    find_packages(&e.path(), results, args, recurse)?;
                 }
                 _ => {}
             }
@@ -109,14 +136,94 @@ fn find_packages(dir: &Path, results: &mut Vec<Entry>, immediate: bool) -> io::R
     return Ok(());
 }
 
+/// Fast colcon list replacement
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct MainArgs {
+    /// Unused, we don't log anything
+    log_base: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Args)]
+struct ListArgs {
+    /// Not implemented
+    #[arg(short = 't', long, default_value_t = false)]
+    topological_order: bool,
+
+    /// Output only the name of each package but not the path
+    #[arg(
+        short = 'n',
+        long,
+        default_value_t = false,
+        conflicts_with = "paths_only"
+    )]
+    names_only: bool,
+
+    /// Output only the path of each package but not the name
+    #[arg(
+        short = 'p',
+        long,
+        default_value_t = false,
+        conflicts_with = "names_only"
+    )]
+    paths_only: bool,
+
+    /// The base paths to recursively crawl for packages
+    #[arg(long, num_args = 0..)]
+    base_paths: Vec<std::path::PathBuf>,
+
+    /// The paths to check for a package. Use shell wildcards (e.g. `src/*`) to select all direct subdirectories
+    /// TODO: we don't do globs yet
+    #[arg(long, num_args = 0..)]
+    paths: Vec<std::path::PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List packages, optionally in topological ordering.
+    List(ListArgs),
+}
+
+fn preprocess_paths(raw: &Vec<std::path::PathBuf>) -> Vec<std::path::PathBuf> {
+    return raw
+        .iter()
+        .dedup()
+        .map(|p| p.canonicalize().unwrap_or(p.clone()))
+        .dedup()
+        .collect();
+}
+
 fn main() -> io::Result<()> {
-    let immediate = true;
-    let mut res = Vec::<Entry>::new();
-    find_packages(Path::new("."), &mut res, immediate)?;
-    if !immediate {
-        res.sort_unstable_by(|a, b| return a.pkg.name.cmp(&b.pkg.name));
-        for e in res {
-            e.print();
+    let args = MainArgs::parse();
+    match &args.command {
+        Commands::List(list_args) => {
+            let mut res = Vec::<Entry>::new();
+
+            let unique_paths = preprocess_paths(&list_args.paths);
+            for to_check in &unique_paths {
+                println!("Checking {:?}", to_check);
+                find_packages(&to_check, &mut res, list_args, false)?;
+            }
+
+            if unique_paths.is_empty() && list_args.base_paths.is_empty() {
+                find_packages(Path::new("."), &mut res, list_args, true)?;
+            } else {
+                for p in preprocess_paths(&list_args.base_paths) {
+                    println!("Checking recursively in {:?}", p);
+                    find_packages(&p, &mut res, list_args, true)?;
+                }
+            }
+
+            for e in res
+                .iter()
+                .sorted_unstable_by_key(|e| (&e.pkg.name, &e.path))
+                .dedup_by(|a, b| a.pkg.name == b.pkg.name && a.path == b.path)
+            {
+                e.print_from_opts(list_args);
+            }
         }
     }
     return Ok(());
